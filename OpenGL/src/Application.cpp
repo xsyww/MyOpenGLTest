d2201d8ca860dc982444305515c957ca0678b5f1
xsyww@163.com <xsyww@163.com>
2020-12-01 20:53:14 +0800

    shader compile

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,6 +1,52 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(GL_VERTEX_SHADER);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+
+        std::cout << "Failed to compile " << 
+            (type == GL_VERTEX_SHADER ? "vertex" : "fragment" ) << 
+            std::endl;
+        std::cout << message << std::endl;
+
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
 
 int main(void)
 {
@@ -45,7 +91,8 @@ int main(void)
         GL_FLOAT, // 每个元素是什么类型
         GL_FALSE, // 是否要单位化
         sizeof(float) * 2,  // 每个buffer元素的长度
-        (const void*)0);    // 当前attribute所在每个元素中的偏移量
+        (const void*)0
+    );    // 当前attribute所在每个元素中的偏移量
 
 
     /* Loop until the user closes the window */
@@ -54,7 +101,11 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+
+
         glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
