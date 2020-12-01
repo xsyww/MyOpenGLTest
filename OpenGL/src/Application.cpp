b867e1453ec457c197f83f1db7e312761cc5cfbb
xsyww@163.com <xsyww@163.com>
2020-12-01 20:09:49 +0800

    glew库引入

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -16,8 +18,35 @@ int main(void)
         return -1;
     }
 
+    glewInit();
+
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error!" << std::endl;
+
+    std::cout << "Program runs in openGL with version: " <<  glGetString(GL_VERSION) << std::endl;
+
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f,
+    };
+
+    unsigned int buffer;    // buffer id
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);   // 启用attrbute
+    glVertexAttribPointer(0, // attribute id
+        2, // 取几个元素 本例中x,y两个
+        GL_FLOAT, // 每个元素是什么类型
+        GL_FALSE, // 是否要单位化
+        sizeof(float) * 2,  // 每个buffer元素的长度
+        (const void*)0);    // 当前attribute所在每个元素中的偏移量
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -25,12 +54,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
