a59086382dc3a82ed480ce44027bf715f1209b71
xsyww@163.com <xsyww@163.com>
2020-12-03 19:49:02 +0800

    lesson24 setup a test framework.

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -21,137 +21,76 @@
 #include "vendor/glm/glm.hpp"
 #include "vendor/glm/gtc/matrix_transform.hpp"
 
+#include "tests/TestClearColor.h"
+
 int main(void)
 {
-    GLFWwindow* window;
+	GLFWwindow* window;
 
-    if (!glfwInit())
-        return -1;
+	if (!glfwInit())
+		return -1;
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
-    glewInit();
+	glewInit();
 
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1);
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
 
-    if (glewInit() != GLEW_OK)
-        std::cout << "Error!" << std::endl;
+	if (glewInit() != GLEW_OK)
+		std::cout << "Error!" << std::endl;
 
-    std::cout << "OpenGL: " <<  glGetString(GL_VERSION) << std::endl;
-
-    {   // 这个作用域用来确保 vb，vi可以在 glfwTerminate() 之前释放掉
-        float positions[] = {
-            -50.0f, -50.0f, 0.0f, 0.0f,   // 左下
-             50.0f, -50.0f, 1.0f, 0.0f,   // 右下
-             50.0f,  50.0f, 1.0f, 1.0f,   // 右上
-            -50.0f,  50.0f, 0.0f, 1.0f,   // 左上
-        };
-
-        unsigned int indices[] = {
-            0, 1, 2,
-            2, 3, 0
-        };
-
-        // 下面两句用来启用贴图透明，在我的电脑上好像不执行也可以。。。。。
-        GLCall(glEnable(GL_BLEND));
-        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-        VertexArray va;
-        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-
-        VertexBufferLayout layout;
-        layout.Push<float>(2);
-        layout.Push<float>(2);
-        va.AddBuffer(vb, layout);
-
-        IndexBuffer ib(indices, 6);
-
-        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-        glm::vec3 translationA(200, 200, 0);
-        glm::vec3 translationB(400, 200, 0);
-
-        Shader shader("res/shaders/Basic.shader");
-        shader.Bind();
-        shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-
-		Texture texture("res/textures/space.png");
-		texture.Bind();
-		shader.SetUniform1i("u_Texture", 0);
-
-        // unbind all
-        va.Unbind();
-        shader.Unbind();
-        vb.Unbind();
-        ib.Unbind();
-
-        Renderer renderer;
-
-		ImGui::CreateContext();
-		ImGui_ImplGlfw_InitForOpenGL(window, true);
-		ImGui_ImplOpenGL3_Init("#version 130");
+	std::cout << "OpenGL: " << glGetString(GL_VERSION) << std::endl;
 
 
-        float f = 0.0f;
-        float r = 0.0f;
-        float increment = 0.05f;
-        /* Loop until the user closes the window */
-        while (!glfwWindowShouldClose(window))
-        {
-            renderer.Clear();
+	GLCall(glEnable(GL_BLEND));
+	GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
-			shader.Bind();
-			//shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-		    //if (r > 1.0f) increment = -0.05f;
-			//else if (r < 0.0f) increment = 0.05f;
-			//r += increment;
+	Renderer renderer;
 
-            {   // obj1
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-                glm::mat4 mvp = proj * view * model;
-                shader.SetUniformMat4f("u_MVP", mvp);
-                renderer.Draw(va, ib, shader);
-            }
-            {   // obj2
-				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-				glm::mat4 mvp = proj * view * model;
-				shader.SetUniformMat4f("u_MVP", mvp);
-				renderer.Draw(va, ib, shader);
-            }
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 130");
 
-			ImGui_ImplOpenGL3_NewFrame();
-			ImGui_ImplGlfw_NewFrame();
-			ImGui::NewFrame();
+	test::TestClearColor test;
 
-			ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
-			ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
-    		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+	while (!glfwWindowShouldClose(window))
+	{
+		renderer.Clear();
 
-			ImGui::Render();
-			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		test.OnUpdate(0.0f);
+		test.OnRender();
 
-            /* Swap front and back buffers */
-            glfwSwapBuffers(window);
-            glfwPollEvents();
-        }
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		test.OnImGuiRender();
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 
 
-		ImGui_ImplOpenGL3_Shutdown();
-		ImGui_ImplGlfw_Shutdown();
-		ImGui::DestroyContext();
-    };
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 
-    glfwTerminate();
-    return 0;
+
+	glfwTerminate();
+	return 0;
 }
